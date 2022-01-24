4ea8b9c8e21e9c824692cae6dda6008095ccdb12
simonmunozbaldo <alfazsimon@gmail.com>
2022-01-24 18:16:50 +0100

    commint on "master"

--- routes/Employees.js
+++ routes/Employees.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = require("../app");
 const router = express.Router();
 router.use(express.json());
 const utils = require("./Utils")
@@ -25,6 +26,21 @@ router.get("/empleado/id/:id",async (req,res) => {
     res.end();
 });
 
+router.get("/empleado/usuario/:usuario",async (req,res) => {
+    let database = app.getDatabase();
+    let collection = database.collection("Empleados");
+    const empleado = await collection.findOne({usuario:req.params.usuario});
+    if(empleado == null){
+        res.sendStatus(404);
+        console.log("Empleado not found")
+
+    }else{
+        res.send(empleado);
+        console.log("Empleado devuelto");
+    }
+    res.end();
+});
+
 //Posts
 router.post("/empleado",async (req,res) => {
     if(await utils.checkUserNameFromEmployeeExists(req.body)){
@@ -42,7 +58,7 @@ router.post("/empleado",async (req,res) => {
 router.put("/empleado",async (req,res) => {
     if(await utils.checkUserNameFromEmployeeExists(req.body)){
         res.send("Error Usuario Ya Existe");
-        console.log("Error Usuario Ya Existe")
+        console.log("Error Usuario con id" + req.body._id +" Ya Existe")
         res.end();
     }else{
         utils.replaceInCollectionById("Empleados",req.body,res,"Empleado editado","No existe ningun empleado con ese id (Update)");
@@ -53,6 +69,7 @@ router.put("/empleado",async (req,res) => {
 router.delete("/empleado/id/:id",async (req,res) => {
     utils.deleteFromCollectionById("Empleados",parseInt(req.params.id),res,"Empleado eliminado","No hay ningun empleado con ese id (Delete)");
 });
+
 
 
 
